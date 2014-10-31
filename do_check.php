25b4458ef6201c13d64f672cc3fa48773fb70afe
viyancs <msofyancs@gmail.com>
2014-10-31 13:59:07 +0700

    update fixing  variable

--- do_check.php
+++ do_check.php
@@ -2,11 +2,11 @@
 
 //declare variable
 $host = 'bandung.microad.co.id';
-$ports = array(22,80,8585)
+$ports = array(22,80,8585);
 foreach ($ports as $port) {
 	$connection = @fsockopen($host,$port);
 	if (is_resource($connection)) {
-		echo '<h2>'. $hosts. ':' . $port . ' ' . '('. getservbyport($port,'tcp'). ') is open.</h2>' . "\n";
+		echo '<h2>'. $host. ':' . $port . ' ' . '('. getservbyport($port,'tcp'). ') is open.</h2>' . "\n";
 		fclose($connection);
 	}
 	else {
