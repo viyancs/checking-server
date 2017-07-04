efd0456950554e58532d082ff0c0eecddc0081e4
viyancs <msofyancs@gmail.com>
2017-07-04 11:36:29 +0700

    Update server.php

--- server.php
+++ server.php
@@ -2,16 +2,16 @@
 require_once __DIR__ . '/emailutil.php';
 Class Server {
 
-   public static function check($host) {
-      $ports = array(22,80,8585,1001,21);
+   public static function check($host,$ports,$name) {
+      $ports = explode(',',$ports);
       foreach ($ports as $port) {
          $connection = @fsockopen($host,$port);
          if (is_resource($connection)) {
 	    echo '<h2>'. $host. ':' . $port . ' ' . '('. getservbyport($port,'tcp'). ') is open.</h2>' . "\n";
             fclose($connection);
             continue;
-         }
-         EmailUtil::send($host,$port);
+         } 
+         EmailUtil::send($host,$port,$name);
       }
    }
 }
