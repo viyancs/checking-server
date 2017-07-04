22c93f1d13264181d757f43e7be9194e9ae459d5
viyancs <msofyancs@gmail.com>
2017-07-04 11:32:27 +0700

    Update do_check.php
    adding port and name server

--- do_check.php
+++ do_check.php
@@ -2,14 +2,19 @@
    require_once __DIR__ . '/server.php';
    //server array
    $hosts = array(
-      'server1',
-      'server2'
+      '203.190.241.130:80,22:"Development Server"',
+      '203.190.243.34:80,22:"Production Microsite"',
+      '192.168.1.225:5432:"Database Postgress Production"',
+      '192.168.1.136:6380:"Redis Server"',
+      
    );
 
    foreach($hosts as $host){
       var_dump($host);
-      Server::check($host);
-      sleep(60);
+      $server = explode(":",$host);
+      $host = $server[0];
+      $port = $server[1];
+      $name = $server[2];
+      Server::check($host,$port,$name);
    }
-
 
